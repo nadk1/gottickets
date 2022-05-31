53ea5d5b0901e7778a3b3c3f69674e15ae859f91
nadk1 <nomadic.nad@icloud.com>
2022-05-31 12:32:47 +0400

    Work on Events Controller and Views

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,19 +1,22 @@
 class EventsController < ApplicationController
+  before_action :set_event, only: %i[show edit update destroy]
+
   def index
     @events = Event.all
+    @categories = Category.all
   end
 
   def show
-    @event = Event.find(:id)
   end
 
   def new
     @event = Event.new
-  #  @categories = Category.all
+    @categories = Category.all
   end
-# Need to add categories to event creation
+
   def create
     @event = Event.new(event_params)
+    @categories = Category.all
     if @event.save
       redirect_to event_path(@event)
     else
@@ -21,9 +24,28 @@ class EventsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    @event.update(event_params)
+
+    redirect_to event_path(@event)
+  end
+
+  def destroy
+    @event.destroy
+
+    redirect_to events_path
+  end
+
   private
 
   def event_params
     params.require(:event).permit(:name, :description, :location, :price, :capacity, :date, :category_id, :user_id)
+  end
+
+  def set_event
+    @event = Event.find(params[:id])
   end
 end
