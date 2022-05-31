b6c946b6da1447c6debd7cc7177f3da75364b0de
nadk1 <nomadic.nad@icloud.com>
2022-05-31 15:02:32 +0400

    Worked on Events controller

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,10 +4,13 @@ class Event < ApplicationRecord
   has_many :reviews
   has_many :bookings
 
+  CATEGORIES = Category.all
+
   validates :name, presence: true, uniqueness: { scope: :date,
                                                  message: "Already an event with that name on this date!" }
   validates :description, presence: true, length: { minimum: 50 }
   validates :location, presence: true
+  validates :category, inclusion: { in: CATEGORIES }
   validates :price, presence: true, numericality: { only_integer: true }
   validates :capacity, presence: true, numericality: { only_integer: true }
   validates :date, presence: true
