2b6f22e87f7afa00188717ecf6a4b5e800f5d657
Jean Philippe Auguste <jeanphilippe.agt@gmail.com>
2022-05-31 12:16:45 +0400

    added the reviews and pages controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,28 @@
 class ReviewsController < ApplicationController
+    def new
+        @review = Review.new
+    end
+
+    def create
+        @review = review.new(review_params)
+        @event = Event.find(params[:event_id])
+        @review.event = @event
+        if @review.save
+            redirect_to event_path(@event)
+        else
+            render :new
+        end
+    end
+
+    def destroy
+        @review = Review.find(params[:id])
+        @review.destroy
+        redirect_to events_path(@event)
+    end
+
+private
+
+    def review_params
+        params.require(:review).permit(:content, :rating)
+    end
 end
