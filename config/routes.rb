c3d3daf90d232a794248f44e586de49fa137dfe1
nadk1 <nomadic.nad@icloud.com>
2022-05-31 17:34:27 +0400

    Work on Bookings

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
   devise_for :users
   resources :events, only: %i[index new create] do
     resources :reviews, only: %i[index new create destroy]
+    resources :bookings, only: %i[new create]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :events, only: :show
   resources :reviews, except: %i[index new create destroy]
   resources :categories, only: :index
+  resources :bookings, except: %i[new create]
 end
