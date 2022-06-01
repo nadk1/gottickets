4c7f19adc44f915314ac5a0af467560e92934e74
nadk1 <nomadic.nad@icloud.com>
2022-06-01 16:18:03 +0400

    Commented through

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ApplicationRecord
-  belongs_to :event, :user, through: :bookings
+  belongs_to :event #, through: :bookings
+  belongs_to :user #, through: :bookings
 
   validates :content, presence: true, length: { minimum: 5 }
 end
