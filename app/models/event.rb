8f48fc381de93d42d5111b8b3503d2aed6abbf91
nadk1 <nomadic.nad@icloud.com>
2022-06-02 10:26:49 +0400

    Added longitude and latitude to Events table

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,6 +5,9 @@ class Event < ApplicationRecord
   has_many :bookings
   has_one_attached :photo
 
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
+
   CATEGORIES = Category.all
 
   validates :name, presence: true, uniqueness: { scope: :date,
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_01_063805) do
+ActiveRecord::Schema.define(version: 2022_06_02_062410) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,8 @@ ActiveRecord::Schema.define(version: 2022_06_01_063805) do
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["category_id"], name: "index_events_on_category_id"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
